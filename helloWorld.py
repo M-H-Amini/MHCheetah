e202dfb9dc6923a39df35230f3527272ac2cda9d
MohammadHossein <m.hossein.amini@gmail.com>
2020-10-02 14:09:51 +0330

    Simple walking simulated and failedgit add helloWorld.py !

--- helloWorld.py
+++ helloWorld.py
@@ -20,7 +20,7 @@ def setBackLeftVelocity(p, q, v):
     p.setJointMotorControl2(q, 23, p.VELOCITY_CONTROL, targetVelocity=-v)
 
 def setFrontRightPosition(p, q, pos, max_v = 10):
-    p.setJointMotorControl2(q, 1, p.POSITION_CONTROL, targetPosition=pos, maxVelocity=max_v)
+    p.setJointMotorControl2(q, 1, p.POSITION_CONTROL, targetPosition=pos ,maxVelocity=max_v)
     p.setJointMotorControl2(q, 4, p.POSITION_CONTROL, targetPosition=-pos, maxVelocity=max_v)
 
 def setFrontLeftPosition(p, q, pos, max_v = 10):
@@ -43,25 +43,23 @@ p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0 ,0 ,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-quadruped = p.loadURDF("quadruped/minitaur.urdf", [-0.000046, -0.000068, 0.200774],
-               [-0.000701, 0.000387, -0.000252, 1.000000],
+quadruped = p.loadURDF("quadruped/minitaur.urdf", [0, 0, 0.4],
+               [1, 1, 0, 0],
                useFixedBase=False)
 nJ = p.getNumJoints(quadruped)
 print('No of joints: ', p.getNumJoints(quadruped))
 for i in range(nJ):
     print('Join info: ', p.getJointInfo(quadruped, i))
 print('No of constraints: ', p.getNumConstraints())
-
+#p.resetBasePositionAndOrientation(quadruped, [0, 0, 0], [0, 1, 0, 0])
 for i in range (10000):
-    #setFrontRightVelocity(p, quadruped, 2)
-    #setFrontLeftVelocity(p, quadruped, 20)
-    #setBackRightVelocity(p, quadruped, 5)
-    #setBackLeftVelocity(p, quadruped, 10)
-    #p.setJointMotorControl2(quadruped, 1, p.POSITION_CONTROL,
-    #      targetPosition=2*np.pi if int(i/100)%2 else 2 * np.pi / 3, maxVelocity=15)
     #pos, orient = p.getBasePositionAndOrientation(quadruped)
-    setFrontRightPosition(p, quadruped, 2 * np.pi * int(i / 50) / 12)
-    setBackLeftPosition(p, quadruped, 2 * np.pi * int(i / 50) / 12)
+    position = (np.pi / 3) * np.sin((2 * np.pi / 100) * i)
+    print('Pos: ',position * 180 / np.pi)
+    setFrontRightPosition(p, quadruped, - (np.pi/6) *  np.sin(np.pi * i / 100))
+    setFrontLeftPosition(p, quadruped, - (np.pi/6) *  np.sin(np.pi * i / 100 + np.pi/4))
+    setBackLeftPosition(p, quadruped, - (np.pi/6) *  np.sin(np.pi * i / 100))
+    setBackRightPosition(p, quadruped, - (np.pi/6) *  np.sin(np.pi * i / 100 + np.pi/4))
     p.stepSimulation()
     time.sleep(1./240.)
 cubePos, cubeOrn = p.getBasePositionAndOrientation(quadruped)
